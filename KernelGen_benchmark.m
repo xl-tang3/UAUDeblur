80a452e5c1c525cabe2d983648c8f272f523c6c3
Xiaole Tang <93478325+xl-tang3@users.noreply.github.com>
2023-10-17 01:16:07 +0800

    Update KernelGen_benchmark.m

--- KernelGen_benchmark.m
+++ KernelGen_benchmark.m
@@ -1,13 +1,13 @@
 % Change the file source correspondingly to make it work
-y = double(imread('.\Datasets\Lai\ker_lai\natural_02_kernel_01.png'))/255;   % true blurry
+y = double(imread('.\Datasets\Levin\ker_levin\im_4_ker_1.png'))/255;   % true blurry
 y1 = y(:,:,1);
 imwrite(y1,'blurry.png');
 y = y1;
-x = double(imread('.\Datasets\Lai\sharp\natural_02.png'))/255;
+x = double(imread('.\Datasets\Levin\sharp\im_4.png'))/255;
 imwrite(x(:,:,1),'clean.png')
 SIZE = size(x(:,:,1));
 % k = double(imread('D:\Pycharm\DRP\data\Levin_NK\BD_cho_and_lee_tog_2009\kernel_estimates\k_2_im_1_cho_and_lee.png'));
-k = double(imread('.\Datasets\Lai\BD_perrone_cvpr_2014\kernel_estimates\psf_natural_02_kernel_01_14_perrone.png'))/255;
+k = double(imread('.\Datasets\Levin\BD_cho_and_lee_tog_2009\kernel_estimates\k_1_im_4_cho_and_lee.png'))/255;
 k = k(:,:,1);
 inacu = k/sum(k(:));
 PSFext = extendHforConv(inacu,SIZE(1),SIZE(2));
