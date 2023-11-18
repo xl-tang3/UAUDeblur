a7549777575976eb37e937d1d476f42da3fe09d5
Xiaole Tang <93478325+xl-tang3@users.noreply.github.com>
2023-11-18 17:47:09 +0800

    Delete Datasets/Inaccurate directory

--- Datasets/Inaccurate/extendHforConv.m
+++
@@ -1,8 +0,0 @@
-function [ConvH] = extendHforConv(H, ny, nx)
-    ConvH = zeros(ny,nx);
-    [m2y,m2x]=size(H);
-    my = floor(m2y/2);
-    mx = floor(m2x/2); %centre is mx+1
-    ConvH(1:m2y,1:m2x) = H;
-    ConvH = circshift(ConvH, [-my -mx]);
-return;
