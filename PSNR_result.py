30406f1c196b61ee3d13f891714fe6ecdfe75204
Xiaole Tang <93478325+xl-tang3@users.noreply.github.com>
2023-11-18 19:26:59 +0800

    Delete PSNR_result.py

--- PSNR_result.py
+++
@@ -1,8 +0,0 @@
-import cv2
-from util import *
-from skimage.metrics import structural_similarity as ssim
-img_clean = cv2.imread("D:/Pycharm/Deblurring/results/im_1.png")
-img_out = cv2.imread("D:/Pycharm/Deblurring/RCTLS/result1.png")
-img_clean = cv2.cvtColor(img_clean, cv2.COLOR_BGR2GRAY)/255.0
-img_out = cv2.cvtColor(img_out, cv2.COLOR_BGR2GRAY)/255.0
-print(compare_psnr(img_clean, img_out),ssim(img_clean, img_out))
